6f458b38982e0e418b434adad2a0103398811a3a
Kim.hyunmook <gusanr4200@naver.com>
2022-07-29 16:27:38 +0900

    init
    1

--- server/mysql/table_sql/user.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE `users` (
-    `num` int(11) NOT NULL AUTO_INCREMENT,
-    `id` varchar(100) NOT NULL,
-    `password` varchar(25) NOT NULL,
-    `address` varchar(20) NOT NULL,
-    `phone` varchar(15) NOT NULL,
-    PRIMARY KEY (num)
-); 
